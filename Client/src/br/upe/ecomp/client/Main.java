0dab81d66bc1611c696b07459a8aa6f7889227c7
serginhofribeiro@gmail.com <serginhofribeiro@gmail.com@9682ea9b-9de5-f2c8-06d0-dbe797032de3>
2011-05-06 03:49:29 +0000

    Compartilhando projeto que utilizará as funções de envio de pacote.

---
+++ Client/src/br/upe/ecomp/client/Main.java
@@ -0,0 +1,20 @@
+/**
+ * Quem vai receber os pacotes.
+ */
+
+package br.upe.ecomp.client;
+
+
+/**
+ * @author Sergio Ribeiro
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+	
+	
+}
